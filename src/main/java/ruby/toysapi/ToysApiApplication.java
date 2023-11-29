68d048340768ab7f8ab7d58b615e52679884bf5b
Ruby <93859705+MisterRuby@users.noreply.github.com>
2023-11-29 18:58:56 +0900

    프로젝트 생성

---
+++ src/main/java/ruby/toysapi/ToysApiApplication.java
@@ -0,0 +1,13 @@
+package ruby.toysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruby/toysapi/ToysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ruby.toysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
